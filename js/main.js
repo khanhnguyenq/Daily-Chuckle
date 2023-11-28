4ee81622cfd03a1cad0fe5cac869daea7e7c40ba
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2023-11-28 00:51:45 +0000

    Added navbar show and hide functionality #1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
-const $category = document.querySelector('.category');
+const $navBar = document.querySelector('.navbar');
+const $allCategory = document.querySelector('.all-category');
+const $savedBtn = document.querySelector('.saved');
+const $barsBtn = document.querySelector('.fa-bars');
+const $xBtn = document.querySelector('.fa-x');
 
-console.log('category btn:', $category);
+$navBar.addEventListener('click', function (event) {
+  if (
+    event.target.tagName === 'I' &&
+    event.target.className === 'fa-solid fa-bars'
+  ) {
+    $allCategory.classList.remove('hidden');
+    $savedBtn.classList.remove('hidden');
+    $xBtn.classList.remove('hidden');
+    $barsBtn.classList.add('hidden');
+  } else if (
+    event.target.tagName === 'I' &&
+    event.target.className === 'fa-solid fa-x'
+  ) {
+    $allCategory.classList.add('hidden');
+    $savedBtn.classList.add('hidden');
+    $xBtn.classList.add('hidden');
+    $barsBtn.classList.remove('hidden');
+  }
+});
