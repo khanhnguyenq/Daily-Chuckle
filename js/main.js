6cb940f0b4e1c676e293ac7b56006f384ceffdfe
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2023-11-28 16:42:41 +0000

    Updated CSS for category buttons and added AJAX functionality to the buttons #1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const $navBar = document.querySelector('.navbar');
-const $allCategory = document.querySelector('.all-category');
+const $allCategoryBtn = document.querySelector('.all-category');
 const $savedBtn = document.querySelector('.saved');
 const $barsBtn = document.querySelector('.fa-bars');
 const $xBtn = document.querySelector('.fa-x');
@@ -9,7 +9,7 @@ $navBar.addEventListener('click', function (event) {
     event.target.tagName === 'I' &&
     event.target.className === 'fa-solid fa-bars'
   ) {
-    $allCategory.classList.remove('hidden');
+    $allCategoryBtn.classList.remove('hidden');
     $savedBtn.classList.remove('hidden');
     $xBtn.classList.remove('hidden');
     $barsBtn.classList.add('hidden');
@@ -17,9 +17,40 @@ $navBar.addEventListener('click', function (event) {
     event.target.tagName === 'I' &&
     event.target.className === 'fa-solid fa-x'
   ) {
-    $allCategory.classList.add('hidden');
+    $allCategoryBtn.classList.add('hidden');
     $savedBtn.classList.add('hidden');
     $xBtn.classList.add('hidden');
     $barsBtn.classList.remove('hidden');
+  }
+});
+
+let text = '';
+
+const $categoryContainer = document.querySelector('.h-category');
+
+$categoryContainer.addEventListener('click', function (event) {
+  event.preventDefault();
+  if ((event.target.tagName = 'BUTTON')) {
+    text = event.target.textContent;
+    console.log('text:', text);
+  }
+  if (text === 'random') {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      console.log(xhr.status);
+      console.log(xhr.response);
+    });
+    xhr.send();
+  } else {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.chucknorris.io/jokes/random?category=' + text);
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      console.log(xhr.status);
+      console.log(xhr.response);
+    });
+    xhr.send();
   }
 });
