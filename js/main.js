6972ffecdd1f92cbb698d47dd03fe6b343719d62
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2023-11-29 23:12:59 +0000

    Performed final clean up for review #2

--- js/main.js
+++ js/main.js
@@ -33,39 +33,42 @@ const $categoryContainer = document.querySelector('.h-category');
 
 $categoryContainer.addEventListener('click', function (event) {
   event.preventDefault();
-  if ((event.target.tagName = 'BUTTON')) {
+  if (event.target.tagName === 'BUTTON') {
     text = event.target.textContent;
+    if (text === 'random') {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
+      xhr.responseType = 'json';
+      xhr.addEventListener('load', function () {
+        const generatedJoke = {};
+        generatedJoke.categories = xhr.response.categories;
+        generatedJoke.value = xhr.response.value;
+        generatedJoke.entryId = data.nextEntryId;
+        data.joke.unshift(generatedJoke);
+        data.nextEntryId++;
+        $jokeArea.appendChild(renderOneJoke(data.joke[0]));
+      });
+      xhr.send();
+    } else {
+      const xhr = new XMLHttpRequest();
+      xhr.open(
+        'GET',
+        'https://api.chucknorris.io/jokes/random?category=' + text,
+      );
+      xhr.responseType = 'json';
+      xhr.addEventListener('load', function () {
+        const generatedJoke = {};
+        generatedJoke.categories = xhr.response.categories;
+        generatedJoke.value = xhr.response.value;
+        generatedJoke.entryId = data.nextEntryId;
+        data.joke.unshift(generatedJoke);
+        data.nextEntryId++;
+        $jokeArea.appendChild(renderOneJoke(data.joke[0]));
+      });
+      xhr.send();
+    }
+    viewSwap('joke');
   }
-  if (text === 'random') {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
-    xhr.responseType = 'json';
-    xhr.addEventListener('load', function () {
-      const generatedJoke = {};
-      generatedJoke.categories = xhr.response.categories;
-      generatedJoke.value = xhr.response.value;
-      generatedJoke.entryId = data.nextEntryId;
-      data.joke.unshift(generatedJoke);
-      data.nextEntryId++;
-      $jokeArea.appendChild(renderOneJoke(data.joke[0]));
-    });
-    xhr.send();
-  } else {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api.chucknorris.io/jokes/random?category=' + text);
-    xhr.responseType = 'json';
-    xhr.addEventListener('load', function () {
-      const generatedJoke = {};
-      generatedJoke.categories = xhr.response.categories;
-      generatedJoke.value = xhr.response.value;
-      generatedJoke.entryId = data.nextEntryId;
-      data.joke.unshift(generatedJoke);
-      data.nextEntryId++;
-      $jokeArea.appendChild(renderOneJoke(data.joke[0]));
-    });
-    xhr.send();
-  }
-  viewSwap('joke');
 });
 
 const $jokeHeader = document.querySelector('.joke-header');
@@ -129,7 +132,7 @@ const $savedJokeArea = document.querySelector('ul');
 
 function renderOneJoke(joke) {
   const $jokeP = document.createElement('p');
-  $jokeP.setAttribute('class', 'flex center-all text-center font-small');
+  $jokeP.setAttribute('class', 'joke-p flex center-all text-center font-small');
   $jokeP.textContent = joke.value;
 
   return $jokeP;
@@ -139,6 +142,7 @@ document.addEventListener('DOMContentLoaded', function () {
   for (let i = 0; i < data.joke.length; i++) {
     $savedJokeArea.appendChild(renderOneJoke(data.joke[i]));
   }
+  viewSwap(data.view);
 });
 
 $savedBtn.addEventListener('click', function () {
@@ -146,6 +150,7 @@ $savedBtn.addEventListener('click', function () {
 });
 
 $homeBtn.addEventListener('click', function () {
-  location.reload();
+  const $jokeP = document.querySelector('.joke-p');
+  $jokeP.remove();
   viewSwap('landing');
 });
